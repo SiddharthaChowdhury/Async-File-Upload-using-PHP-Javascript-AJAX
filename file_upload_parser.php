245f0dff299189a7f0dc1d464d0822b59cb4a844
Siddhartha Chowdhury <siddhartha.chowdhury@akademe.in>
2016-07-18 13:44:08 +0530

    Init

---
+++ file_upload_parser.php
@@ -0,0 +1,16 @@
+<?php
+$fileName = $_FILES["file1"]["name"]; // The file name
+$fileTmpLoc = $_FILES["file1"]["tmp_name"]; // File in the PHP tmp folder
+$fileType = $_FILES["file1"]["type"]; // The type of file it is
+$fileSize = $_FILES["file1"]["size"]; // File size in bytes
+$fileErrorMsg = $_FILES["file1"]["error"]; // 0 for false... and 1 for true
+if (!$fileTmpLoc) { // if file not chosen
+    echo "ERROR: Please browse for a file before clicking the upload button.";
+    exit();
+}
+if(move_uploaded_file($fileTmpLoc, "test_uploads/$fileName")){
+    echo "$fileName upload is complete";
+} else {
+    echo "move_uploaded_file function failed";
+}
+?>
